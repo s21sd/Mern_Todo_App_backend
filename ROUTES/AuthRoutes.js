3174da64e3289fdea12cc509415a3bdb57a2b780
s21sd <srivastavasunny359@gmail.com>
2024-03-01 22:19:40 +0530

    dsdsds

--- ROUTES/AuthRoutes.js
+++ ROUTES/AuthRoutes.js
@@ -40,7 +40,7 @@ router.post('/register', async (req, res) => {
     }
 })
 
-// // Login API 
+
 router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
